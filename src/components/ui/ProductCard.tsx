d27009fa575924e4f7fbcbe6841abc7c41d624a1
Vlad <vlad.lemkov@yandex.ru>
2024-09-25 00:44:09 +0400

    add data

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -1,5 +1,5 @@
-import Skeleton from "react-loading-skeleton";
-import { useState } from "react";
+import Skeleton from 'react-loading-skeleton';
+import { useState } from 'react';
 interface Props {
 	data: Car;
 }
@@ -10,9 +10,7 @@ export interface Car {
 	characteristics?: any;
 	package?: string;
 }
-const ProductCard = (
-	{ data }: Props
-) => {
+const ProductCard = ({ data }: Props) => {
 	const [isImageLoaded, setIsImageLoaded] = useState(false);
 	return (
 		<div className='product-card'>
@@ -26,8 +24,27 @@ const ProductCard = (
 						onLoad={() => setIsImageLoaded(true)}
 					/>
 				</div>
-				<div className='product-card__model'>
-					{`${data.characteristics.year} BMW ${data.model}`}
+				<div className='product-card__car-data'>
+					<div className='product-card__car-data-model'>
+						{`${data.characteristics.year} BMW ${data.model}`}
+					</div>
+					<div className='product-card__car-data-characteristics'>
+						<div className='car-data-characteristics__characteritics-item'>
+							Body: <span>{data.characteristics.body}</span>
+						</div>
+						<div className='car-data-characteristics__characteritics-item'>
+							Exterior Color:{' '}
+							<span>{data.characteristics.exteriorColor}</span>
+						</div>
+						<div className='car-data-characteristics__characteritics-item'>
+							Drive Line:{' '}
+							<span>{data.characteristics.driveLine}</span>
+						</div>
+						<div className='car-data-characteristics__characteritics-item'>
+							Fuel Type:{' '}
+							<span>{data.characteristics.fuelType}</span>
+						</div>
+					</div>
 				</div>
 				<div className='product-card__price'>
 					{new Intl.NumberFormat('de-DE', {
